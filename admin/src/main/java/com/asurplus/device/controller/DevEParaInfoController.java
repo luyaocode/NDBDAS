3f701e4237dea7e4b3a936c5e0371dc4e4ffa0e2
luyao <316774040@qq.com>
2022-09-21 18:26:33 +0800

    前端页面完善

--- admin/src/main/java/com/asurplus/device/controller/DevEParaInfoController.java
+++ admin/src/main/java/com/asurplus/device/controller/DevEParaInfoController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import static com.asurplus.App.socks;
 
 /**
  * 电参量信息表 前端控制器
+ *
  * @author luyao
  * @since 2022/9/13
  */
@@ -43,20 +46,73 @@ public class DevEParaInfoController {
 //    }
 
     /**
-     * 仅查询时间最近一条的记录
+     * 查询当前电参量
+     *
      * @return one
      */
     @SaCheckPermission("system:role:list")
     @GetMapping("A01-01")
-    public List<DevEParaInfo> getNowTime() {
-        List<DevEParaInfo> list=new ArrayList<>();
+    public DevEParaInfo getNow() {
+        DevEParaInfo info = new DevEParaInfo();
         //构造查询条件
-        QueryWrapper<DevEParaInfo> wrapper=new QueryWrapper<>();
+        QueryWrapper<DevEParaInfo> wrapper = new QueryWrapper<>();
         wrapper.orderByDesc("create_time");
         wrapper.last("limit 1");
-        list.add(devEParaInfoService.getOne(wrapper));
-        System.out.println("返回数据："+list);
+        info = devEParaInfoService.getOne(wrapper);
+        System.out.println("返回当前电参量：" + info);
+        return info;
+    }
+
+    /**
+     * 查询历史电参量
+     *
+     * @return all
+     */
+    @SaCheckPermission("system:role:list")
+    @GetMapping("A01-01-all")
+    public List<DevEParaInfo> getAll() {
+        List<DevEParaInfo> list = new ArrayList<>();
+        //构造查询条件
+        QueryWrapper<DevEParaInfo> wrapper = new QueryWrapper<>();
+//        按照时间正序
+        wrapper.orderByAsc("create_time");
+//        wrapper.last("limit 1");
+        list = devEParaInfoService.list(wrapper);
+        System.out.println("返回历史电参量：" + list);
         return list;
+    }
+
+    /**测试用
+     * 插入一条记录
+     * 电压0-300
+     * 电流0-100
+     */
+    @SaCheckPermission("system:role:list")
+    @GetMapping("A01-01-add")
+    public String saveOne() {
+//        System.out.println("++++++++++++++++++测试+++++++++++++++++++++");
+        DevEParaInfo info = new DevEParaInfo();
+        Random rand = new Random();
+        float voltA = 220+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*10;
+        float voltB = 220+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*10;
+        float voltC = 220+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*10;
+        float currA = 50+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*5;
+        float currB = 50+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*5;
+        float currC = 50+rand.nextFloat() * (rand.nextBoolean() ? 1 : -1)*5;
+
+        info.setVoltA(voltA);
+        info.setVoltB(voltB);
+        info.setVoltC(voltC);
+        info.setCurrA(currA);
+        info.setCurrB(currB);
+        info.setCurrC(currC);
+
+        info.setDevId("A01-01");
+        info.setDevName("第一台测试设备，自动生成数据");
+        info.setCreateTime(new Date());
+        boolean save = devEParaInfoService.save(info);
+        System.out.println(save ? "插入成功！" : "插入失败！");
+        return (save ? "插入成功！" : "插入失败！");
     }
 
     /**
@@ -64,14 +120,14 @@ public class DevEParaInfoController {
      */
     @SaCheckPermission("system:role:list")
     @PostMapping("A01-01/0x03")
-    public String sendCommand(@RequestBody String command){
+    public String sendCommand(@RequestBody String command) {
 //        取第一个socket
-        Socket sock=socks[0];
-        String sendStr="0808 0000 0006 01 03 9c41 0001";
-        System.out.println("[0x03] "+command);
+        Socket sock = socks[0];
+        String sendStr = "0808 0000 0006 01 03 9c41 0001";
+        System.out.println("[0x03] " + command);
 //        System.out.println(sock);
-        TcpConnection.send(sock,sendStr);
-        return sock+sendStr+"命令帧发送成功！";
+        TcpConnection.send(sock, sendStr);
+        return sock + sendStr + "命令帧发送成功！";
 
     }
 
--- ui/src/api/device/dev.js
+++ ui/src/api/device/dev.js
@@ -2,14 +2,24 @@ import request from '@/utils/request'
 
 const baseUrl = '/device/dev-epara-info/';
 
-// 查询设备显示数据
-export function listEPara() {
+// 查询当前电参数
+export function getEParas() {
   return request({
     url: baseUrl + 'A01-01',
     method: 'get',
     // params: query
   })
 }
+
+//查询历史电参量
+export function ListEParas() {
+  return request({
+    url: baseUrl + 'A01-01-all',
+    method: 'get',
+    // params: query
+  })
+}
+
 
 //读单个线圈
 export function readHoldingRegister(data) {
@@ -18,5 +28,17 @@ export function readHoldingRegister(data) {
     method: 'post',
     data: data
     // params: query
+  })
+}
+
+
+/**
+ * 测试用
+ */
+//插入一条电参量记录
+export function testAddEPara(){
+  return request({
+    url:baseUrl+"A01-01-add",
+    method:'get',
   })
 }
